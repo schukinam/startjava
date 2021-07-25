0cf510b7cb8e3d79d96f8e09ccff0bf4d34ed272
schukinam <and.schukin@gmail.com>
2021-07-25 14:56:05 +0300

    fixed formatting errors; deleted args

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,9 +1,9 @@
-public class ConditionalStatement{
-    public static void main(String[] args){
-        int age = Integer.parseInt(args[0]);
-        boolean isMaleGender = Boolean.parseBoolean(args[1]);
-        float height = Float.parseFloat(args[2]);
-        char firstLetterOfName = args[3].charAt(0);
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        int age = 21;
+        boolean isMaleGender = false;
+        float height = 1.6f;
+        char firstLetterOfName = 'I';
 
         if(age > 20) {
             System.out.println("Возраст " + age + " больше 20");
@@ -18,14 +18,14 @@ public class ConditionalStatement{
         }
 
         if(height < 1.80) {
-            System.out.println("Рост " + height + " меньше 1.80");
+            System.out.println("Рост меньше 1.80");
         } else {
-            System.out.println("Рост " + height + " не меньше 1.80");
+            System.out.println("Рост не меньше 1.80");
         }
 
-        if(firstLetterOfName == "M".charAt(0)) {
+        if(firstLetterOfName == 'M') {
             System.out.println("Первая буква имени действительно M");
-        } else if(firstLetterOfName == "I".charAt(0)) {
+        } else if(firstLetterOfName == 'I') {
             System.out.println("Первая буква имени действительно I");
         } else {
             System.out.println("Первая буква имени равна " + firstLetterOfName);
