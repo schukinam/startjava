d9db8603182e2f9956c68c0435cc595b34847f93
schukinam <and.schukin@gmail.com>
2021-07-25 15:17:03 +0300

    fixed variable declaration

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,28 +1,27 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 21;
-        boolean isMaleGender = false;
-        float height = 1.6f;
-        char firstLetterOfName = 'I';
-
         if(age > 20) {
             System.out.println("Возраст " + age + " больше 20");
         }
 
+        boolean isMaleGender = false;
         if(isMaleGender) {
             System.out.println("Пол мужской");
         }
-
+        
         if(!isMaleGender) {
             System.out.println("Пол не мужской");
         }
 
+        float height = 1.6f;
         if(height < 1.80) {
             System.out.println("Рост меньше 1.80");
         } else {
             System.out.println("Рост не меньше 1.80");
         }
 
+        char firstLetterOfName = 'I';
         if(firstLetterOfName == 'M') {
             System.out.println("Первая буква имени действительно M");
         } else if(firstLetterOfName == 'I') {
